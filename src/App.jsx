4b7fe9eeb71c63ffed9003521c1a0c2b786f35ef
Ajay Dayalani <ajaydayalani@MacBook-Pro.broadband>
2024-07-24 10:07:11 +0100

    fix links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, HashRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import {Home, Projects,Contact,About} from './pages'
 
